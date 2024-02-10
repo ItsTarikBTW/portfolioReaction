3d616c81c3307ee34631377c3cbaec915fcc19d0
tarik <77856978+ItsTarikBTW@users.noreply.github.com>
2024-02-10 18:28:36 +0100

    Refactor ModeToggle component and update global styles

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,13 +1,6 @@
 import * as React from "react"
-import { Moon, Sun } from "lucide-react"
-
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { Button } from "@/components/ui/button";
 
 export function ModeToggle() {
   const [theme, setThemeState] = React.useState<
@@ -28,25 +21,25 @@ export function ModeToggle() {
   }, [theme])
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setThemeState("theme-light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setThemeState("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setThemeState("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    
+    <Button
+    variant="outline"
+    size="icon"
+    onClick={
+      theme === "dark" ? () => setThemeState("theme-light") : () => setThemeState("dark")
+    }
+  >
+    <SunIcon
+      className={`h-[1.2rem] w-[1.2rem]  transition-all  ${
+        theme === "dark" ? "-rotate-90 scale-0" : "rotate-0 scale-100"
+      }`}
+    />
+    <MoonIcon
+      className={`absolute h-[1.2rem] w-[1.2rem] transition-all ${
+        theme === "dark" ? "rotate-0 scale-100" : "-rotate-90 scale-0"
+      }`}
+    />
+    <span className="sr-only">Toggle theme</span>
+  </Button>
   )
 }
