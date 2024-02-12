e07287209a6f1e8d1fd9dd2ee97cd1fc0bf8326e
tarik <77856978+ItsTarikBTW@users.noreply.github.com>
2024-02-12 22:17:22 +0100

    Refactor CustomHoverCard component to include platform icon

--- src/components/CustomHoverCard.tsx
+++ src/components/CustomHoverCard.tsx
@@ -9,14 +9,36 @@ import {
 import { Button } from "@/components/ui/button";
 
 interface CustomHoverCardProps {
-  children: ReactNode;
+  key: number | string;
+  platform: string;
+  data: {
+    handler: string;
+    description: string;
+    name: string;
+    url: string;
+    d: string;
+  };
 }
 
-export const CustomHoverCard: React.FC<CustomHoverCardProps> = ({ children }) => {
+export const CustomHoverCard: React.FC<CustomHoverCardProps> = ({
+  key,
+  platform,
+  data,
+}) => {
   return (
     <HoverCard>
       <HoverCardTrigger asChild>
-        <a href="/">{children}</a>
+        <a href="/">
+          <svg
+            className="h-5 w-5"
+            role="img"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>{platform}</title>
+            <path fill="currentColor" d={data.d}></path>
+          </svg>
+        </a>
       </HoverCardTrigger>
       <HoverCardContent className="w-80">
         <div className="flex justify-between space-x-4">
